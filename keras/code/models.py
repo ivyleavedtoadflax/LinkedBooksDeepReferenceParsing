fb371c44ba6bcd6a9c3e79bc6446ee659204f620
Matthew Upson <matthew.a.upson@gmail.com>
2019-08-13 00:20:38 +0100

    chg: Add logging

--- keras/code/models.py
+++ keras/code/models.py
@@ -24,8 +24,24 @@ from sklearn_crfsuite import metrics
 
 # Utils script
 from code.utils import *
+import logging
 
+# Set up logging
 
+LOGGING_LEVEL = os.getenv("LOGGING_LEVEL")
+
+if isinstance(LOGGING_LEVEL, str):
+    numeric_level = getattr(logging, LOGGING_LEVEL.upper(), 10)
+else:
+    numeric_level = 20
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(
+    format="%(asctime)s %(name)s %(levelname)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=numeric_level,
+)
 
 def BiLSTM_model(filename, train, output,
               X_train, X_test, word2ind, maxWords,
